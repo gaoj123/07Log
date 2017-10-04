c15ffdbc2039d15de6b9d317476609b5b8f25186
Jenny Gao <jgao658@gmail.com>
2017-10-04 19:02:03 -0400

    added session

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
 app=Flask(__name__)
+app.secret_key="THIS IS NOT SECURE"
 
 @app.route("/")
 def root():
-    return render_template("form.html")
+    if 'Name' in session:
+        return  render_template("response.html", name=session['Name'], message="WELCOME!") ##User is already logged in, so shows Welcome page
+    else:
+        return render_template("form.html") ##User is not logged in
 
 @app.route("/response", methods=["POST","GET"])
 def response():
@@ -13,21 +17,29 @@ def response():
     print request.form
     username=request.form["Name"]
     password=request.form["Password"]
-    status=validate(username,password)
+    status=validate(username,password) ##Checks if username and password are correct
     if status==0:
-        return render_template("response.html", name=username)
+        session['Name']=username
+        print "Session: "+session['Name']
+        return render_template("response.html", name=username, message="WELCOME!") #username and password are correct, so shows Welcome page
     elif status==1:
-        return render_template("form.html",message="Wrong Password")
+        return render_template("form.html",message="Wrong Password") #redirects to login page and shows message saying that password entered is wrong
     else:
-        return render_template("form.html",message="Wrong Username")
+        return render_template("form.html",message="Wrong Username") #redirects to login page and shows message saying that username is wrong
     
 
 def validate(username, password):
-    if username == "DW":
-        if password == "kittens":
+    if username == "DW": ##correct username
+        if password == "kittens": ##correct password
             return 0 #everything correct
         return 1 #wrong password
     return 2 # wrong username
+
+@app.route("/logout", methods=["POST","GET"])
+def logout():
+    if 'Name' in session:
+        session.pop('Name') #remove user who was just logged in from session
+    return render_template("logout.html") 
 
 if __name__=="__main__":
     app.debug=True
